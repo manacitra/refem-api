ae69e1be2033a3d2e3dd08cbfa12825dea607f21
triwjy <ztriwijaya@gmail.com>
2018-11-11 23:01:13 +0800

    combine ss mapper

--- app/models/entities/paper.rb
+++ app/models/entities/paper.rb
@@ -6,22 +6,12 @@ module RefEm
     class Paper < Dry::Struct
       include Dry::Types.module
 
-<<<<<<< HEAD
       attribute :id,          Strict::Integer.optional
       attribute :title,       Strict::String
       attribute :author,      Strict::Array.of(String)
       attribute :year,        Strict::Integer.optional
       attribute :date,        Strict::String.optional
       attribute :field,       Strict::Array.of(String).optional
-=======
-      attribute :id,          Integer.optional
-      attribute :origin_id,   Strict::Integer
-      attribute :title,       Strict::String
-      attribute :author,      Strict::String
-      attribute :year,        Strict::Integer
-      attribute :date,        Strict::String
-      attribute :field,       Strict::String
->>>>>>> 6132d80d6cd864a4363a7328a27d264fa65ce1fb
       attribute :doi,         Strict::String.optional
       attribute :citation_velocity,            Strict::Integer.optional
       attribute :citation_dois,                Strict::Array.of(String).optional # sometimes doi is null
