733da9e8dc6e97202cdecb150f8bf3a999fc2ec8
triwjy <ztriwijaya@gmail.com>
2018-12-02 11:46:39 +0800

    copy from monolit

--- app/presentation/assets/js/customize.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function($) {
-    $("#search_keyword").click(function() {
-        $("#paper_query_input").attr("placeholder", "e.g. 'internet'");
-    });
-
-    $("#search_title").click(function() {
-        $("#paper_query_input").attr("placeholder", "e.g. 'the internet of things a survey'");
-    });
-});
